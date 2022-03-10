6e905f5ce54c3ea75687aaf553ac1031d4a7a152
Black_Gonza <87800784+BlackGonza@users.noreply.github.com>
2022-03-10 18:46:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# BMI calculation software
+
+from colorama import init, Fore, Back, Style
+init()
+
+print(Back.GREEN + "Hi there, welcome to BMI calculation software by Black_Gonza!")
+print(Back.CYAN)
+weight = float(input("What's your weight?: "))
+height = float(input("And what's your height?: "))
+print("")
+
+bmi = float("{0:.2f}".format(weight / ((height / 100) * (height / 100))))
+
+print(Back.RESET + "Your current BMI is " + str(bmi))
+
+if( bmi <= 16):
+	print(Back.RED + "High body mass deficit", end = '')
+
+if( bmi >= 16 and bmi <= 18.5):
+	print(Back.YELLOW + "(Insufficient body weight", end = '')
+
+if( bmi >= 18.5 and bmi <= 25):
+	print(Back.GREEN + "(Your weight is OK", end = '')
+
+if( bmi >= 25 and bmi <= 30):
+	print(Back.YELLOW + "(Excess Body weight", end = '')
+
+if( bmi >= 30 and bmi <= 35):
+	print(Back.RED + "(Obesity of the 1 degree", end = '')
+
+if( bmi >= 35 and bmi <= 40):
+	print(Back.RED + "(Obesity of the 2 degree", end = '')
+
+if( bmi > 40):
+	print(Back.RED + "(Obesity of the 3 degree (morbid)", end = '')
+
+print(")",end = '')
+print(Style.RESET_ALL)
+input()
